c28082c8ac1a920f8cdd7100cc4571e438315fa1
erikgreasy <erik.masny@gmail.com>
2020-04-24 16:27:51 +0200

    added functionality for variable product and also simplified code

--- index.php
+++ index.php
@@ -15,64 +15,61 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 
 
-// ================================== //
-//             SETTINGS               //
-// ================================== //
-
-$cat_name = "Sale";
-
-
-
 
 // ================================== //
 //          ADD TO SALE CAT           //
 // ================================== //
 
-add_action( 'save_post_product', 'add_product_to_sale_cat' );
+add_action( 'save_post_product', 'handle_product_to_sale_cat' );
 
-function add_product_to_sale_cat( $post_id ) {
-    global $cat_name;
+function handle_product_to_sale_cat( $post_id ) {
+
+    // SET THIS TO YOUR SALE CATEGORY NAME
+    $cat_name = 'Sale';
+
+
+    // IF AUTOSAVE THEN -> DO NOTHING
     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
         return $post_id;
     }
 
+    // IF USER DONT HAVE PERMISSIONS TO EDIT PRODUCT -> DO NOTHING
     if ( ! current_user_can( 'edit_product', $post_id ) ) {
         return $post_id;
     }
 
-    if( get_post_status( $post_id ) == 'publish' && isset($_POST['_sale_price']) ) {
-        $sale_price = $_POST['_sale_price'];
 
-        if( $sale_price >= 0 && ! has_term( $cat_name, 'product_cat', $post_id ) ){
-            wp_set_object_terms($post_id, $cat_name, 'product_cat', true );
+
+    $product = new WC_Product_Variable( $post_id );
+
+    // PRODUCT WITH VARIATIONS
+    if( $product->has_child() ) {
+        if (get_post_status( $post_id ) == 'publish') {
+
+            if( $product->is_on_sale() ) {
+                wp_set_object_terms($post_id, $cat_name, 'product_cat', true );
+                
+            } else {
+                wp_remove_object_terms( $post_id, $cat_name, 'product_cat' );
+        
+            }
+        }
+        
+    // PRODUCT WITHOUT VARIATIONS
+    } else {
+
+        if( get_post_status( $post_id ) == 'publish' && isset($_POST['_sale_price']) ) {
+            $sale_price = $_POST['_sale_price'];
+            
+            // has sale price
+            if( $sale_price >= 0 && ! has_term( $cat_name, 'product_cat', $post_id ) ){
+                wp_set_object_terms($post_id, $cat_name, 'product_cat', true );
+            }
+            // doesnt have sale price
+            if ( $sale_price == '' && has_term( $cat_name, 'product_cat', $post_id ) ) {
+                wp_remove_object_terms( $post_id, $cat_name, 'product_cat' );
+            }
         }
     }
-}
 
-
-
-// ================================== //
-//       REMOVE FROM SALE CAT         //
-// ================================== //
-
-
-add_action( 'save_post_product', 'remove_product_from_sale_cat' );
-
-function remove_product_from_sale_cat( $post_id ) {
-    global $cat_name;
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-        return $post_id;
-    }
-    if ( ! current_user_can( 'edit_product', $post_id ) ) {
-        return $post_id;
-    }
-    if( get_post_status( $post_id ) == 'publish' && isset($_POST['_sale_price']) ) {
-        $sale_price = $_POST['_sale_price'];
-
-        if( $sale_price >= 0 && ! has_term( $cat_name, 'product_cat', $post_id ) ){
-            wp_set_object_terms($post_id, $cat_name, 'product_cat', true );
-        } elseif ( $sale_price == '' && has_term( $cat_name, 'product_cat', $post_id ) ) {
-            wp_remove_object_terms( $post_id, $cat_name, 'product_cat' );
-        }
-    }
 }
